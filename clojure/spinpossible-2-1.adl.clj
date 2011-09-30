b97baa410ca4ac22009ddb54b76f0d07dfd38e33
baltar <bastian@blankenburg.net>
2011-09-30 18:40:49 +0200

    fixed adl example

--- clojure/spinpossible-2-1.adl.clj
+++ clojure/spinpossible-2-1.adl.clj
@@ -1,15 +1,18 @@
 (define (domain spinpossible-2-1)
-  (:requirements :adl :types)
+  (:requirements :adl :typing)
   (:types position tile)
   (:predicates
     (upright ?t - tile)
-    (at ?p - position ?t - tile))
+    (at ?p - position ?t - tile)
+    (adjunct ?p0 ?p1 - position))
   
   (:action 
+    flip
     :parameters (?t - tile)
     :effect
-      (when (upright ?t) (not (upright ?t)))
-      (when (not (upright ?t)) (upright ?t)))
+      (and
+        (when (upright ?t) (not (upright ?t)))
+        (when (not (upright ?t)) (upright ?t))))
              
   (:action 
     move-0-1
@@ -17,12 +20,14 @@
                  ?t0 ?t1 - tile)
     :precondition
       (and
-        (at ?p0 ?t0) (at ?p1 ?t1))
+        (at ?p0 ?t0) (at ?p1 ?t1)
+        (adjunct ?p0 ?p1))
     :effect
       (and
-        (at ?p0 ?t1) (not (at ?p0 ?t0))´
+        (at ?p0 ?t1) (not (at ?p0 ?t0))
         (at ?p1 ?t0) (not (at ?p1 ?t1))
-        (forall (?t - tile) 
-                (when (upright ?t) (not (upright ?t)))
-                (when (not (upright ?t)) (upright ?t))))))
+        (when (upright ?t0) (not (upright ?t0)))
+        (when (not (upright ?t0)) (upright ?t0))
+        (when (upright ?t1) (not (upright ?t1)))
+        (when (not (upright ?t1)) (upright ?t1)))))
         
--- clojure/spinpossible_test_2_1_adl.clj
+++ clojure/spinpossible_test_2_1_adl.clj
@@ -3,7 +3,7 @@
   (:objects
     p0 p1 - position
     t0 t1 - tile)
-  (:init 
+  (:init (adjunct p0 p1)
          (at p0 t1) (upright t1) (at p1 t0) (upright t0))
   (:goal (and (at p0 t0) (upright t0) (at p1 t1) (upright t1))))
 
