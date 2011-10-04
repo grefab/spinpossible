827e2f61765ce07d1df4aa787e05b694dd0f3e43
vale <vale1410@gmail.com>
2011-10-04 09:11:54 +0200

    organized stuff

--- asp/statistic.sh
+++ asp/statistic.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 echo gregor
-for x in {0..24}; do echo $x; ./run.sh data/gregor8.lp $x 300 >> output/output9.txt; done
+#for x in {0..24}; do echo $x; ./run.sh data/gregor8.lp $x 300 >> output/output9.txt; done
 echo mareike
-for x in {0..24}; do echo $x; ./run.sh data/mareike.lp $x 600 >> output/output9.txt; done
+#for x in {0..24}; do echo $x; ./run.sh data/mareike.lp $x 600 >> output/output9.txt; done
 
 # 100 randomly generated puzzles
-for p in data/100/*; 
+for p in data/gen/*; 
 do
    echo $p
    for x in {0..24}; 
    do
        echo $x
-      ./run.sh $p $x 300 >> output/output9.txt ;
+      ./run.sh $p $x 300 >> output/output_gen.txt ;
    done;
 done;
