86e71b86fd26de6d10dd148ef04720d609633283
vale <vale1410@gmail.com>
2012-04-16 21:45:48 +0200

    output organized and cnf added.

--- asp/do.sh
+++ asp/do.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
-./start.sh asp4 benchmark/l9_48_SAT.txt > output_l9_48_SAT_asp4.txt 
-./start.sh asp4 benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_asp4.txt 
-./start.sh asp4 benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_asp4.txt 
-
-
 ./start.sh asp3 benchmark/l9_48_SAT.txt > output_l9_48_SAT_asp3.txt 
 ./start.sh asp3 benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_asp3.txt 
 ./start.sh asp3 benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_asp3.txt 
 
-./start.sh sat benchmark/l9_48_SAT.txt > output_l9_48_SAT_sat.txt 
-./start.sh sat benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_sat.txt 
-./start.sh sat benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_sat.txt 
+#./start.sh asp4 benchmark/l9_48_SAT.txt > output_l9_48_SAT_asp4.txt 
+#./start.sh asp4 benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_asp4.txt 
+#./start.sh asp4 benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_asp4.txt 
+
+
+#./start.sh sat benchmark/l9_48_SAT.txt > output_l9_48_SAT_sat.txt 
+#./start.sh sat benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_sat.txt 
+#./start.sh sat benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_sat.txt 
 
