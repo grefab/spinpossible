5a171dbd1b1cfe55a5ee76c626234c8db4d3ae77
vale <vale1410@gmail.com>
2012-04-20 11:09:32 +0700

    improved asp3

--- asp/compute_total.sh
+++ asp/compute_total.sh
@@ -2,7 +2,7 @@
 
 tmp=tmp/numbers.tmp
 
-cat $1 | grep '^c* *Time' | sed 's/^c* *Time *: \([0-9]*\).[0-9]*s (Solving: [0-9]*.[0-9]*s 1st Model: \([0-9]*\).*/\1/g' > \
+cat $1 | grep '^c* *Time' | sed 's/^c* *Time *: \([0-9]*.[0-9]*\)s (Solving: [0-9]*.[0-9]*s 1st Model: \([0-9]*\).*/\1/g' > \
 $tmp
 
 sum=0; 
