574c1ac5f46787c69bbd0d68a3cd677a2edff857
vale <vale1410@gmail.com>
2012-04-23 14:55:07 +0700

    asp5 added. New encoding by martin Gebser

--- asp/do.sh
+++ asp/do.sh
@@ -1,13 +1,17 @@
 #!/bin/zsh
 
+
+./start.sh asp5 benchmark/l9_48_SAT.txt > output_l9_48_SAT_asp5.txt 
+./start.sh asp5 benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_asp5.txt 
+./start.sh asp5 benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_asp5.txt 
+
+./start.sh asp4 benchmark/l9_48_SAT.txt > output_l9_48_SAT_asp4.txt 
+./start.sh asp4 benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_asp4.txt 
+./start.sh asp4 benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_asp4.txt 
+
 ./start.sh asp3 benchmark/l9_48_SAT.txt > output_l9_48_SAT_asp3.txt 
 ./start.sh asp3 benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_asp3.txt 
 ./start.sh asp3 benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_asp3.txt 
-
-#./start.sh asp4 benchmark/l9_48_SAT.txt > output_l9_48_SAT_asp4.txt 
-#./start.sh asp4 benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_asp4.txt 
-#./start.sh asp4 benchmark/l8_92_unique_SAT.txt > output_l8_92_unique_SAT_asp4.txt 
-
 
 #./start.sh sat benchmark/l9_48_SAT.txt > output_l9_48_SAT_sat.txt 
 #./start.sh sat benchmark/l8_48_UNSAT.txt > output_l8_48_UNSAT_sat.txt 
